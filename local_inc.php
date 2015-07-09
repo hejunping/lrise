7d58f77a23e265d484a895813d00082d3ae1ba14
fedor.jia <332762488@qq.com>
2015-07-10 00:59:59 +0800

    no message

--- local_inc.php
+++ local_inc.php
@@ -1,10 +1,12 @@
 <?php
 $is_open_local = true;
 $array_local = array(
-    'DB_HOST'  => 'localhost',  //服务器地址 // TODO localhost
+//     'DB_HOST'  => '127.0.0.1',  //服务器地址
+	'DB_HOST'  => 'localhost',  //服务器地址
 	'DB_NAME'  => 'lrise',   		//数据库名
 	'DB_USER'  => 'root',       //用户名
-	'DB_PWD'   => 'lrise.COM', //密码 // TODO lrise.COM
+// 	'DB_PWD'   => '', //密码
+	'DB_PWD'   => 'lrise.COM', //密码
 );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@ change(TODO):
 	/index.php
 	local_inc.php
 	core/Lib/Action/Home/UserAction.php : 247
+	tpl/head.html: 24 
